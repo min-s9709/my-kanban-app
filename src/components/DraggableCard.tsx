a278565477b3a6e23bb8741763eb030109c552e4
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-03-25 20:16:26 +0900

    delete 버튼 추가

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -3,6 +3,9 @@ import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
 const Card = styled.div<{ isDragging: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   border-radius: 5px;
   padding: 10px 10px;
   margin-bottom: 5px;
@@ -10,6 +13,23 @@ const Card = styled.div<{ isDragging: boolean }>`
     props.isDragging ? "#74b9ff" : props.theme.cardColor};
   box-shadow: ${(props) =>
     props.isDragging ? "0px 2px 5px rgba(0,0,0,0.05)" : "none"};
+`;
+
+const DeleteBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 100%;
+  background-color: lightgray;
+  color: black;
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+  }
 `;
 
 interface IDraggableCardProps {
@@ -29,6 +49,7 @@ const DraggableCard = ({ toDoId, toDoText, index }: IDraggableCardProps) => {
           {...magic.dragHandleProps}
         >
           {toDoText}
+          <DeleteBtn>x</DeleteBtn>
         </Card>
       )}
     </Draggable>
